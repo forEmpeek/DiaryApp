715f2b91ad36f9a51148029faafc674052552ff3
Oleksii <oleksii_baliuk@yahoo.com>
2018-08-04 17:24:36 +0300

    test

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name diaryAppApp
+ * @description
+ * # diaryAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('diaryAppApp', []);
